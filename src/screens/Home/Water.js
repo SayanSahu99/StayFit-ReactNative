1b28def269996f65f2feadba2b7a6f2bf9296a84
SayanSahu99 <sayansovan@gmail.com>
2022-06-09 13:20:06 +0530

    Water Intake screen

---
+++ src/screens/Home/Water.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { View} from 'react-native';
+
+
+const Water = () => {
+
+    return (
+        <View>
+      
+        </View>
+    )
+}
+
+
+export default Water;
