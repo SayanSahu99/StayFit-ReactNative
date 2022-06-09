7f42aa2b47d84187ccb1eb7595a324fa4ec81df1
SayanSahu99 <sayansovan@gmail.com>
2022-06-09 13:18:29 +0530

    Home screen

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import { View } from 'react-native'
+
+export default function Home({ navigation }) {
+  // TODO: add firebase sign-out and user info function later
+    
+  return (
+    <View>
+    </View>
+
+  )
+}
