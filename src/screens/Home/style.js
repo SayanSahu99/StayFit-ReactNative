84ccb1f4afaa3f09b380a3b55225013ed4870e4f
SayanSahu99 <sayansovan@gmail.com>
2022-06-09 13:19:39 +0530

    Stylesheet styles

---
+++ src/screens/Home/style.js
@@ -0,0 +1,33 @@
+const React = require("react-native");
+
+export default {
+
+containerView: {
+    flex: 1, 
+    justifyContent: 'flex-start', 
+    alignItems:'center',
+    marginTop:20
+},
+
+TextView: {
+    margin: 12
+},
+
+greetingText:{
+    marginBottom: 10
+},
+
+listView:{
+    marginTop: 20,
+},
+
+listText: {
+    fontSize: 25,
+    fontWeight: 'bold'
+},
+
+listSubText: {
+    fontSize: 14
+}
+
+};
