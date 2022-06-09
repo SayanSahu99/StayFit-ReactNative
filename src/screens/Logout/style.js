13773bde8ab7b691e3e7383d16d5d272eecf2b54
SayanSahu99 <sayansovan@gmail.com>
2022-06-09 13:20:47 +0530

    Logout screen

---
+++ src/screens/Logout/style.js
@@ -0,0 +1,42 @@
+const React = require("react-native");
+
+export default {
+
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 5,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+      button: {
+        marginHorizontal: 20
+      },
+      textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: 'bold'
+      }
+
+
+};
