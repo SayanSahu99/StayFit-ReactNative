1b9886ba22b7bbcc9786b566c44745382e29c326
SayanSahu99 <sayansovan@gmail.com>
2022-06-09 13:28:54 +0530

    Conected Navigation and Redux

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigation from './src/navigations/AppNavigation';
+import { LogBox } from 'react-native';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './src/Redux/configureStore';
+
+const MyTheme = {
+  colors: {
+    primary: '#1E88E5',
+    background: '#ffffff',
+  },
+};
 
 export default function App() {
+
+  const { store } = ConfigureStore();
+  
+  LogBox.ignoreAllLogs();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+        <NavigationContainer theme={MyTheme}>
+          <AppNavigation />
+        </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
