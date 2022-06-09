3b01fa80924e27c8944d89a5d85e68a4f362efa0
SayanSahu99 <sayansovan@gmail.com>
2022-06-09 13:19:10 +0530

    Food Search screen

---
+++ src/screens/Home/SearchFood.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+
+export default function SearchFood({ navigation }) {
+
+    return (
+        <View>
+        </View>
+    )
+}
