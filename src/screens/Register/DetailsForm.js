38837cb74e57e1f1dd2785159987c49e96bf72d9
SayanSahu99 <sayansovan@gmail.com>
2022-06-09 13:21:58 +0530

    DetailsForm Screen

---
+++ src/screens/Register/DetailsForm.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export const DetailsForm = props => {
+
+    
+    return (
+        <View>
+        </View>
+    );
+}
