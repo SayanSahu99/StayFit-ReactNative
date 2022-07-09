6a8e2f4fcbe81b50504347ef787eb6f0dc07c891
SayanSahu99 <sayansovan@gmail.com>
2022-07-09 19:50:48 +0530

    Food Reducer

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -4,13 +4,16 @@ import logger from 'redux-logger';
 import { Auth } from './auth';
 import { User } from './users';
 import { Health } from './health';
+import { Food } from './food';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             auth: Auth,
             user: User,
-            health: Health
+            health: Health,
+            food: Food
         }),
         applyMiddleware(thunk, logger)
     );
